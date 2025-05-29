b00c2dc4dc780e1f0d428054026aafa1e3010cde
vyngochi <ncv.ngovy@gmail.com>
2025-05-30 01:12:13 +0700

    index

--- index.php
+++ index.php
@@ -1,35 +1,35 @@
 <?php
 session_start();
 require_once "pdo.php";
+
+$stmt = $pdo->query("SELECT profile_id, first_name, last_name, headline FROM Profile");
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-<html>
-<head>
-    <title>Vy Ngo Chi - 161a79c2</title>
-</head>
-<body>
-<h1>Welcome to the Profile Database</h1>
-<?php flashMessages(); ?>
+
+<h1>Resume Registry</h1>
+
 <?php
-$stmt = $pdo->query("SELECT profile_id, first_name, last_name FROM Profile");
-echo "<ul>\n";
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo "<li>";
-    echo '<a href="view.php?profile_id='.$row['profile_id'].'">';
-    echo htmlentities($row['first_name'].' '.$row['last_name']);
-    echo "</a> ";
-    if (isset($_SESSION['name'])) {
-        echo '<a href="edit.php?profile_id='.$row['profile_id'].'">Edit</a> ';
-        echo '<a href="delete.php?profile_id='.$row['profile_id'].'">Delete</a>';
-    }
-    echo "</li>\n";
-}
-echo "</ul>\n";
-if (isset($_SESSION['name'])) {
-    echo '<p><a href="add.php">Add New Entry</a></p>';
-    echo '<p><a href="logout.php">Logout</a></p>';
-} else {
-    echo '<p><a href="login.php">Please log in</a></p>';
+if (isset($_SESSION['success'])) {
+    echo "<p style='color:green'>".$_SESSION['success']."</p>";
+    unset($_SESSION['success']);
 }
 ?>
-</body>
-</html>
+
+<?php if (isset($_SESSION['name'])): ?>
+    <a href="logout.php">Logout</a>
+    <table>
+        <?php foreach ($rows as $row): ?>
+            <tr>
+                <td><a href="view.php?profile_id=<?= $row['profile_id'] ?>"><?= htmlentities($row['first_name'].' '.$row['last_name']) ?></a></td>
+                <td><?= htmlentities($row['headline']) ?></td>
+                <td>
+                    <a href="edit.php?profile_id=<?= $row['profile_id'] ?>">Edit</a> /
+                    <a href="delete.php?profile_id=<?= $row['profile_id'] ?>">Delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    <a href="add.php">Add New Entry</a>
+<?php else: ?>
+    <a href="login.php">Please log in</a>
+<?php endif; ?>
